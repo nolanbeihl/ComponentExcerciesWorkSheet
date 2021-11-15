efea915ce31590a0999d9b4626f249e4bb24e2ef
unknown <nolan.beihl@gmail.com>
2021-11-15 14:27:30 -0700

    Start of worksheet for components

---
+++ App.jsx
@@ -0,0 +1,34 @@
+//Exercise 1
+state = {
+    firstName: 'Reggie',
+    lastName: 'White'
+}
+
+//Exercise 2
+state = {
+    names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kellie']
+}
+
+//Exercise 4
+state = {
+    superheroes = [
+        {
+            superheroId: 1,
+            name: 'Batman',
+            primaryAbility: 'Wealthy',
+            secondarAbility: 'Rich'
+        },
+        {
+            superheroId: 2,
+            name: 'Superman',
+            primaryAbility: 'Super strength',
+            secondarAbility: 'Fly'
+        },
+        {
+            superheroId: 3,
+            name: 'Spiderman',
+            primaryAbility: 'Spider senses',
+            secondarAbility: 'Shoots web'
+        }
+    ]
+}

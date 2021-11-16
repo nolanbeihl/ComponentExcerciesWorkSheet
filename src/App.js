d829165c404929f7218fcb97581bdf919b7b9298
unknown <nolan.beihl@gmail.com>
2021-11-16 14:16:21 -0700

    Deleted app.jsx because I didn't think it was required.  Still trying to figure out inital setup of the app and how to make components correctly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplayName from './components/DisplayName';
+
+
 
 function App() {
+  //Exercise 1
+  this.state = {
+    firstName: 'Reggie',
+    lastName: 'White',
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+          <DisplayName firstName={this.state.firstName}/>
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/components/DisplayName.jsx
+++ src/components/DisplayName.jsx
@@ -1,10 +1,48 @@
+import { Component } from "react";
 import React from 'react';
 
-const DisplayName = (props) => {
-    return (
-        <h1>
-            <Apps name={props.firstName} />
 
-        </h1>
-    );
+// const DisplayName = (props) => {
+//     return (
+//         <h1>firstName={this.App.props.firstName} lastName={this.App.props.lastName}</h1>
+//     );
+// }
+// export default DisplayName;
+
+class DisplayName extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            firstName: '',
+            lastName: ''
+         }
+    }
+
+handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+        [event.target.name]: event.target.value
+    });
 }
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.displayName(this.state)
+}
+
+    render() { 
+        return ( 
+            <form onSubmit={this.handleSubmit}>
+                <label>Please enter your first name</label>
+                <input name="firstName" onChange={this.handleChange} value={this.state.firstName}/>
+                <label>Please enter your last name</label>
+                <input name="lastName" onChange={this.handleChange} value={this.state.lastName}/>
+                <button type="submit">Add to list</button>
+            </form>
+         );
+    }
+}
+ 
+
+export default DisplayName;
